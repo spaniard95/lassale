82bea85c4e2775f0816bd212f540fdd1fd2476f8
spaniard95 <panagiotis.spanos95@gmail.com>
2025-07-24 21:15:22 +0300

    fix prod 2

--- services/expenses/services.ts
+++ services/expenses/services.ts
@@ -1,21 +1,21 @@
 import { GetCategoriesResponse, GetExpensesResponse } from "./types";
 
+const BASE_URL = process.env.BE_URL || "https://murat.deno.dev";
+
 export async function fetchAllExpenses(): Promise<GetExpensesResponse> {
-  const response = await fetch(`${process.env.BE_URL}/expenses/all`);
+  const response = await fetch(`${BASE_URL}/expenses/all`);
   return await response.json();
 }
 
 export async function fetchExpensesByMonth(
   month: number
 ): Promise<GetExpensesResponse> {
-  const response = await fetch(
-    `${process.env.BE_URL}/expenses/all/date?month=${month}`
-  );
+  const response = await fetch(`${BASE_URL}/expenses/all/date?month=${month}`);
   return await response.json();
 }
 
 export async function fetchCategoriesAndSubcategories(): Promise<GetCategoriesResponse> {
-  const response = await fetch(`${process.env.BE_URL}/expenses/categories`);
+  const response = await fetch(`${BASE_URL}/expenses/categories`);
 
   return await response.json();
 }
